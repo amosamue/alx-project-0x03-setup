db1dbe3ca60b626b9f1212fcb790a1df40b11260
amosamue <samuelamoh130@gmail.com>
2025-09-22 04:07:07 +0000

    Fix: import globals.css correctly in _app.tsx

--- alx-project-0x03/pages/_app.tsx
+++ alx-project-0x03/pages/_app.tsx
@@ -1,6 +1,8 @@
-import type { AppProps } from 'next/app'
-import '@/styles/global.css'
+import type { AppProps } from "next/app";
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
